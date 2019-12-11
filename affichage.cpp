aba86f4c9b1eb01dac4808d67fa4ae3c30a43187
Enzo <enzo.allemano@hotmail.com>
2019-12-11 14:39:04 +0100

    Création des fichiers
    reglesDuJeu.cpp
    affichage.cpp

---
+++ affichage.cpp
@@ -0,0 +1,4 @@
+//
+// Created by jetca on 11.12.2019.
+//
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "affichage.cpp"
+#include "reglesDuJeu.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
