eeb0dc7a20e0a61f9f924ceb28ceb54731271da3
Enzo <enzo.allemano@hotmail.com>
2019-12-12 14:04:53 +0100

    règles du jeu,h

---
+++ reglesDuJeu.h
@@ -0,0 +1,9 @@
+//
+// Created by jetca on 12.12.2019.
+//
+#ifndef LAB008_REGLESDUJEU_H
+#define LAB008_REGLESDUJEU_H
+
+enum etatCellule {nonExistant, existant, vide};
+
+#endif //LAB008_REGLESDUJEU_H
