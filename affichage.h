1fe75670dcde55c92a121dcd71ff8756cb354c5a
Isotech42 <jonathan.stocchetti@heig-vd.ch>
2019-12-19 08:57:35 +0100

    Version finale affichage.h
    J'ai oublié au dernier commit de mettre affichage.h

--- affichage.h
+++ affichage.h
@@ -1,3 +1,14 @@
+/// \file affichage.h
+/// \authors Enzo Allemano, Nicolas Viotti, Jonathan Stocchetti
+/// \date 18.12.2019
+///
+/// \brief \b Laboratoire \b 08
+/// \brief Déclare la fonction affichageTablier
+///
+/// Remarque(s) : 
+///
+/// Compilateur :  MinGW-g++ 6.3.0
+
 #ifndef LAB8_AFFICHAGE_H
 #define LAB8_AFFICHAGE_H
 
